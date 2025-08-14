d57fb1962c135fcf51afb762f7db1c58dabcde35
trob94 <trobins945754@gmail.com>
2025-08-14 18:52:20 -0500

    Tro initial branch (#3)
    * Create using_imports.py
    
    * Update using_imports.py
    
    * Create loan.py

---
+++ PythonCourse/using_imports.py
@@ -0,0 +1,8 @@
+# Import the random module (built-in)
+import random
+
+# Generate a random number between 1 and 5
+number = random.randint(1, 5)  # randint includes both ends
+
+# Print the random number
+print("Your random number is:", number)
